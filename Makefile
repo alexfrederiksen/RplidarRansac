32c6e7d721af0ef9afd21c178d81e310c6c3fd7e
Alex Frederiksen <ajfred154@gmail.com>
2018-01-25 20:16:34 -0500

    Add primitive drawing support

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 INCLUDES="./include"
-SDL=-D_REENTRANT -I/usr/include/SDL2 -lSDL2
+#SDL=-D_REENTRANT -I/usr/include/SDL2 -lSDL2
 LSDL=-lSDL2
 OBJECTS = main.o lidarManager.o lidar.a
 
 lidar: core.o bin
-	g++ -std=c++11 -o bin/lidar core.o -pthread $(SDL)
+	g++ -std=c++11 -o bin/lidar core.o -pthread $(LSDL)
 
 core.o:core.cpp
 	g++ -std=c++11 -c core.cpp -I$(INCLUDES)
@@ -13,5 +13,5 @@ bin:
 	mkdir bin
 
 clean:
-	rm *.o
+	rm -f *.o
 	rm -rf bin
--- core.cpp
+++ core.cpp
@@ -1,16 +1,23 @@
 #include "core.h"
 
-void Core::loop() {
+void Core::update() {
+	// do cool stuff here
 }
 
 void Core::render() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+    // render some pretty lines
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    draw_line(-1.0f, -1.0f, 1.0f, 1.0f);
+    draw_line(-1.0f, 1.0f, 1.0f, -1.0f);
+    // render a gorgeous point
+    draw_point(0.0f, 0.0f);
     SDL_RenderPresent(renderer);
 }
 
-void Core::event(SDL_Event& e) {
-    if(e.type == SDL_QUIT) {
+void Core::on_event(SDL_Event& e) {
+    if(e.type == SDL_QUIT || e.type == SDL_KEYDOWN) {
         running = false;
     }
 }
@@ -19,13 +26,13 @@ int Core::run() {
     if(init() == false) 
         return -1;
 
-    SDL_Event e;
+    SDL_Event event;
 
     while(running) {
-        while(SDL_PollEvent(&e)) {
-            event(e);
+        while(SDL_PollEvent(&event)) {
+            on_event(event);
         }
-        loop();
+        update();
         render();
     }
     return 0;
@@ -34,6 +41,9 @@ int Core::run() {
 Core::Core() {
     sDisplay = NULL;
     running = true;
+
+    point_rect.w = POINT_SIZE;
+    point_rect.h = POINT_SIZE;
 }
 
 bool Core::init() {
@@ -43,8 +53,34 @@ bool Core::init() {
     return SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0;
 }
 
+/*
+ * Convert coordinates from device space [-1, 1] to screen space [0, DIMENSION].
+ * In device space, (-1, -1) is the bottom left corner.
+ */
+void Core::to_screen_coords(float & x, float & y) {
+	x = 0.5f * (x + 1.0f) * SCREEN_WIDTH;
+	y = 0.5f * (-y + 1.0f) * SCREEN_HEIGHT;
+}
 
-int main(int, char**) {
+void Core::draw_point(float x, float y) {
+	// convert to screen coords
+	to_screen_coords(x, y);
+	// compute rectangle center
+	point_rect.x = (int) (x - point_rect.w * 0.5f);
+	point_rect.y = (int) (y - point_rect.h * 0.5f);
+	// draw to screen
+	SDL_RenderFillRect(renderer, &point_rect);
+}
+
+void Core::draw_line(float x1, float y1, float x2, float y2) {
+	// convert to screen coords
+	to_screen_coords(x1, y1);
+	to_screen_coords(x2, y2);
+	// draw to screen
+	SDL_RenderDrawLine(renderer, (int) x1, (int) y1, (int) x2, (int) y2);
+}
+
+int main(int, char **) {
     Core core;
-    core.run();
+    return core.run();
 }
